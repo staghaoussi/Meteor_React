efaa6b3568b61780560ccd6a711abf8bf199f3f9
Sylvain <staghaoussi@gmail.com>
2022-05-11 00:30:37 -0700

    first commit to repository, still incomplete

---
+++ my_project-react/server/main.js
@@ -0,0 +1,89 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/db/TasksCollection.js';
+import { ServiceConfiguration } from 'meteor/service-configuration';
+import '/imports/api/tasksMethods';
+
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task',
+    ].forEach(taskText => insertTask(taskText, user));
+  }
+},
+
+ServiceConfiguration.configurations.upsert(
+  { service: 'github' },
+  {
+    $set: {
+      loginStyle: 'popup',
+      clientId: '16910d3a874369eafe1b', // insert your clientId here
+      secret: '3d6745ec453b4a687c245bdbfb73b477a33d8983', // insert your secret here
+    },
+  }
+)
+);
+
+
+
+
+
+
+// code for links example
+
+// import { Meteor } from 'meteor/meteor';
+// import { LinksCollection } from '/imports/api/links';
+
+// function insertLink({ title, url }) {
+//   LinksCollection.insert({title, url, createdAt: new Date()});
+// }
+
+// Meteor.startup(() => {
+//   // If the Links collection is empty, add some data.
+//   if (LinksCollection.find().count() === 0) {
+//     insertLink({
+//       title: 'Do the Tutorial',
+//       url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+//     });
+
+//     insertLink({
+//       title: 'Follow the Guide',
+//       url: 'http://guide.meteor.com'
+//     });
+
+//     insertLink({
+//       title: 'Read the Docs',
+//       url: 'https://docs.meteor.com'
+//     });
+
+//     insertLink({
+//       title: 'Discussions',
+//       url: 'https://forums.meteor.com'
+//     });
+//   }
+// });
